41bcc0c11109557efc57e36b00a34facc214744b
MAforgood <hosseinkhosroabadi45@gmail.com>
2024-01-28 23:27:50 +0330

    commit 2

--- user.h
+++ user.h
@@ -23,6 +23,10 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+// Our Changes
+int example(void);
+int clone(void *stack);
+int join();
 
 // ulib.c
 int stat(const char*, struct stat*);
